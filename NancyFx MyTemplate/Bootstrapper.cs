8e7f262c8cd1acfab69bb3f12401c4328630dfe3
rabitarochan <rabitarochan@gmail.com>
2013-09-20 12:39:32 +0900

    First commit.

---
+++ NancyFx MyTemplate/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using Nancy.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFx_MyTemplate
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            this.Conventions.StaticContentsConventions.Add(Nancy.Conventions.StaticContentConventionBuilder.AddDirectory("assets"));
+        }
+    }
+}
---
+++ NancyFx MyTemplate/Modules/HomeModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFx_MyTemplate.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index"];
+            };
+        }
+    }
+}
